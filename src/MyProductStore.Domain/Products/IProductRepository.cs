ea7b2c622aa985b62dc716a6b477ee406cbaf7cf
Galip Tolga Erdem <galiperdem@gmail.com>
2022-06-27 22:21:21 +0300

    Added custom Product repository with custom method

---
+++ src/MyProductStore.Domain/Products/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Volo.Abp.Domain.Repositories;
+
+namespace MyProductStore.Products;
+
+public interface IProductRepository : IRepository<Product, Guid>
+{
+    Task<List<Product>> GetListByAvailability(bool isAvailable, CancellationToken cancellationToken = default);
+}
---
+++ src/MyProductStore.EntityFrameworkCore/Products/EfCoreProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyProductStore.EntityFrameworkCore;
+using Volo.Abp.Domain.Repositories.EntityFrameworkCore;
+using Volo.Abp.EntityFrameworkCore;
+
+namespace MyProductStore.Products;
+
+public class EfCoreProductRepository : EfCoreRepository<MyProductStoreDbContext, Product, Guid>, IProductRepository
+{
+    public EfCoreProductRepository(IDbContextProvider<MyProductStoreDbContext> dbContextProvider) : base(
+        dbContextProvider)
+    {
+    }
+
+    public async Task<List<Product>> GetListByAvailability(bool isAvailable,
+        CancellationToken cancellationToken = default)
+    {
+        var dbSet = await GetDbSetAsync();
+        return await dbSet
+            .Where(q => q.IsAvailable == isAvailable)
+            .ToListAsync(GetCancellationToken(cancellationToken));
+    }
+}
