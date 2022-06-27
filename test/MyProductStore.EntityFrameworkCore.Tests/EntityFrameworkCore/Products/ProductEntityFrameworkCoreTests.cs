a389db8ed5268ecc6a5fa5dd314c166fcdc81c3a
Galip Tolga Erdem <galiperdem@gmail.com>
2022-06-27 22:21:38 +0300

    Added custom repository test

---
+++ test/MyProductStore.EntityFrameworkCore.Tests/EntityFrameworkCore/Products/ProductEntityFrameworkCoreTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using MyProductStore.Products;
+using Shouldly;
+using Xunit;
+
+namespace MyProductStore.EntityFrameworkCore.Products;
+
+public class ProductEntityFrameworkCoreTests : MyProductStoreEntityFrameworkCoreTestBase
+{
+    private readonly IProductRepository _productRepository;
+
+    public ProductEntityFrameworkCoreTests()
+    {
+        _productRepository = GetRequiredService<IProductRepository>();
+    }
+
+    [Fact]
+    public async Task ProductRepository_Should_Return_Available_Items()
+    {
+        var availableItems = await _productRepository.GetListByAvailability(true);
+        
+        // Because test data is already seeded from the ProductDataSeedContributor
+        availableItems.ShouldNotBeEmpty();
+    }
+}
