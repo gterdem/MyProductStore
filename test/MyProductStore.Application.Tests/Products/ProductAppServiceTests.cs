84033c4d654709dd7b072cd7ad2283a6264cb715
Galip Tolga Erdem <galiperdem@gmail.com>
2022-06-27 22:23:31 +0300

    Added test for product appservice custom method

---
+++ test/MyProductStore.Application.Tests/Products/ProductAppServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Shouldly;
+using Xunit;
+
+namespace MyProductStore.Products;
+
+public class ProductAppServiceTests:MyProductStoreApplicationTestBase
+{
+    private readonly IProductAppService _productAppService;
+
+    public ProductAppServiceTests()
+    {
+        _productAppService = GetRequiredService<IProductAppService>();
+    }
+
+    [Fact]
+    public async Task Service_Should_Get_Unavailable_Items()
+    {
+        var result = await _productAppService.GetProductsByAvailabilityAsync(false);
+        
+        // Seeded test product data is available
+        result.ShouldBeEmpty();
+    }
+}
