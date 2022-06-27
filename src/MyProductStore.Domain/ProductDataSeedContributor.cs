57c89fd8cbc70f7e94fd3f472e5a6dabd30ea237
Galip Tolga Erdem <galiperdem@gmail.com>
2022-06-27 22:20:56 +0300

    Added ProductDataSeedContributor for test product seeding

---
+++ src/MyProductStore.Domain/ProductDataSeedContributor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading.Tasks;
+using MyProductStore.Products;
+using Volo.Abp.Data;
+using Volo.Abp.DependencyInjection;
+using Volo.Abp.Domain.Repositories;
+
+namespace MyProductStore;
+
+public class ProductDataSeedContributor : IDataSeedContributor, ITransientDependency
+{
+    private readonly IRepository<Product, Guid> _productRepository;
+
+    public ProductDataSeedContributor(IRepository<Product, Guid> productRepository)
+    {
+        _productRepository = productRepository;
+    }
+
+    public async Task SeedAsync(DataSeedContext context)
+    {
+        if (await _productRepository.GetCountAsync() == 0)
+        {
+            await CreateTestProductAsync();
+        }
+    }
+
+    private async Task CreateTestProductAsync()
+    {
+        var testProduct = new Product
+        {
+            Name = "Test Product",
+            Price = new decimal(14.50),
+            IsAvailable = true
+        };
+
+        await _productRepository.InsertAsync(testProduct);
+    }
+}
